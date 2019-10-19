3875ae4612052afc2f5e510b7a88be54ae508a26
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-19 20:09:03 +0700

    deploy server to heroku, deploy web to netlify

--- deploy_server_do.sh
+++ deploy_server_do.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 yarn build:server
-docker build -t lesliedinh95/abb:latest .
-docker push lesliedinh95/abb:latest
-ssh root@167.99.75.93 "docker pull lesliedinh95/abb:latest && docker tag lesliedinh95/abb:latest dokku/abb:latest && dokku tags:deploy abb latest"
+heroku container:push --app=nameless-beach-24911 web
+heroku container:release --app=nameless-beach-24911 web
+# docker build -t lesliedinh95/abb:latest .
+# docker push lesliedinh95/abb:latest
+# ssh root@167.99.75.93 "docker pull lesliedinh95/abb:latest && docker tag lesliedinh95/abb:latest dokku/abb:latest && dokku tags:deploy abb latest"
